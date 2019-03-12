e490734b301cb0c6ca19b49585dfa053145cec39
Steve Ellis <email@steveell.is>
2019-03-12 19:32:56 -0400

    add tests for Buffer.appendUint8

--- test/TestBuffer.sol
+++ test/TestBuffer.sol
@@ -14,4 +14,13 @@ contract TestBuffer {
         buf.append("world!");
         Assert.equal(string(buf.buf), "Hello, world!", "Unexpected buffer contents.");
     }
+
+    function testBufferAppendUint8() public {
+        Buffer.buffer memory buf;
+        Buffer.init(buf, 256);
+        buf.append("Hello,");
+        buf.appendUint8(0x20);
+        buf.append("world!");
+        Assert.equal(string(buf.buf), "Hello, world!", "Unexpected buffer contents.");
+    }
 }
