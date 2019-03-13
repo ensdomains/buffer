41003f67ad410f49715bd067437d5eee987d8516
Steve Ellis <email@steveell.is>
2019-03-13 15:18:15 -0400

    add tests for Buffer.append(bytes)

--- test/TestBuffer.sol
+++ test/TestBuffer.sol
@@ -23,4 +23,43 @@ contract TestBuffer {
         buf.append("world!");
         Assert.equal(string(buf.buf), "Hello, world!", "Unexpected buffer contents.");
     }
+
+    function testBufferResizeAppendUint8() public {
+        Buffer.buffer memory buf;
+        Buffer.init(buf, 32);
+        buf.append("01234567890123456789012345678901");
+        buf.appendUint8(0x20);
+        Assert.equal(buf.capacity, 64, "Expected buffer capacity to be 64");
+        Assert.equal(buf.buf.length, 33, "Expected buffer length to be 33");
+        Assert.equal(string(buf.buf), "01234567890123456789012345678901 ", "Unexpected buffer contents");
+    }
+
+    function testBufferResizeAppendBytes() public {
+      Buffer.buffer memory buf;
+      Buffer.init(buf, 32);
+      buf.append("01234567890123456789012345678901");
+      buf.append("23");
+      Assert.equal(buf.capacity, 96, "Expected buffer capacity to be 96");
+      Assert.equal(buf.buf.length, 34, "Expected buffer length to be 33");
+      Assert.equal(string(buf.buf), "0123456789012345678901234567890123", "Unexpected buffer contents");
+    }
+
+    function testBufferResizeAppendManyBytes() public {
+      Buffer.buffer memory buf;
+      Buffer.init(buf, 32);
+      buf.append("01234567890123456789012345678901");
+      buf.append("0123456789012345678901234567890101234567890123456789012345678901");
+      Assert.equal(buf.capacity, 192, "Expected buffer capacity to be 192");
+      Assert.equal(buf.buf.length, 96, "Expected buffer length to be 96");
+      Assert.equal(string(buf.buf), "012345678901234567890123456789010123456789012345678901234567890101234567890123456789012345678901", "Unexpected buffer contents");
+    }
+
+    function testBufferZeroSized() public {
+      Buffer.buffer memory buf;
+      Buffer.init(buf, 0);
+      buf.append("first");
+      Assert.equal(buf.capacity, 32, "Expected buffer capacity to be 32");
+      Assert.equal(buf.buf.length, 5, "Expected buffer length to be 5");
+      Assert.equal(string(buf.buf), "first", "Unexpected buffer contents");
+    }
 }
